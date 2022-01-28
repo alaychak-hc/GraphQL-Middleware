91a53bf220ae657065a49d7a8c7779ae49de9036
Andrew Laychak <alaychak@harriscomputer.com>
2022-01-28 09:51:51 -0500

    fix: :bug: removed alias'

--- packages/middleware/src/index.ts
+++ packages/middleware/src/index.ts
@@ -1,3 +1,25 @@
-import { Timeout, DepthLimit } from '@middleware/All';
+// #region Developer Information
+/*
+ ********************************************
+    Author: Andrew Laychak
+    Email: ALaychak@harriscomputer.com
 
+    Created At: 01-28-2022 09:50:55 AM
+    Last Modified: 01-28-2022 09:51:11 AM
+    Last Updated By: Andrew Laychak
+
+    Description: Index file for the middleware package
+
+    References:
+      - None
+ ********************************************
+*/
+// #endregion
+
+// #region Imports
+import { Timeout, DepthLimit } from './middleware/All';
+// #endregion
+
+// #region Exports
 export { Timeout, DepthLimit };
+// #endregion
